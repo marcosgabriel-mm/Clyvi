632e8175720aeff2b2e958697b7451991363544b
Marcos Gabriel de Mesquita Mauricio <mgmesquitamaurcio@gmail.com>
2023-04-18 22:23:18 -0300

    testes com funções

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from entrada_no_sistema.entrada_no_sistema import escolher_opcoes
+from entrada_usuario.entrada_usuario import escolher_opcoes
+from manipulação_bd.atualizar_arquivo import atualizar_documento
+from manipulação_bd.conectar_db import conectar_ao_banco_de_dados
 
-#chama a função pra inciar o sistema
-escolher_opcoes()
+#Inicia o Sistema
+#escolher_opcoes()
+
+#chama a função de atualizar, mas precisa arrumar
+atualizar_documento(conectar_ao_banco_de_dados(),"Marcos Gabriel")
