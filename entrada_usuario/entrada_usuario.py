446ab52f4e961ceb3ff7fbf5823e1b2878a0a8d2
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2023-04-20 14:55:26 -0300

    removido o import do arquivo verificar_credenciais.

--- entrada_usuario/entrada_usuario.py
+++ entrada_usuario/entrada_usuario.py
@@ -1,23 +1,22 @@
-from .verificar_credenciais import verificar_login
-
 from manipulação_bd.conectar_db import Banco
 
 
 def login_no_sistema():
-    # só fica nesse laço caso o email ou senha esteja errado
+
+    banco = Banco()
+
     while 1:
         e_mail = input("Digite seu E-mail: ")
         senha = input("Digite sua Senha: ")
 
         # verifica se o login está certo
-        if verificar_login(e_mail, senha):
+        if banco.verificar_credenciais(e_mail, senha) == True:
             print("\nBem Vindo de Volta\n")
             return 0
         else:
             print("\nE-Mail ou Senha errado\nDigite Novamente\n")
 
 
-# ainda não terminei
 def criar_conta():
     nome = input("Digite seu nome: ")
     data_de_nascimento = input("Digite sua data de nascimento (D/M/A): ")
@@ -32,9 +31,10 @@ def criar_conta():
 
     banco = Banco()
 
-    banco.inserir_usuarios(credenciais)
-    return credenciais
-
+    if (banco.verificar_user_existente(credenciais)) == True:
+        banco.inserir_usuarios(credenciais)
+    else:
+        print("Já existe uma conta com esse email. Considere fazer login")
 
 # opções que podem ser escolhidas de inicio
 opcoes = {1: login_no_sistema, 2: criar_conta}
