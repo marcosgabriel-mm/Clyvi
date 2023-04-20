e76d5b1c5da36542d5f1b000b6ed5c6791497cd5
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2023-04-20 14:54:37 -0300

    Duas funções feitas: verificar credenciais para o login; e verificar usuario existente, para a criaçãod e usuário

--- manipulação_bd/conectar_db.py
+++ manipulação_bd/conectar_db.py
@@ -11,7 +11,7 @@ class Banco:
             server_api=ServerApi("1"),
         )
 
-    #Essa função nao tá pronta, ela vai apenas procurar e o usuário existe
+    # Essa função provavelmente vai servir de muita coisa, já que retorna todos os usuários
     def usuarios(self):
         banco_de_dados = self.cliente["ClyviDB"]
         colecao = banco_de_dados["usuarios"]
@@ -21,6 +21,29 @@ class Banco:
         banco_de_dados = self.cliente["ClyviDB"]
         colecao = banco_de_dados["usuarios"]
         colecao.insert_one(*args)
+
+    def verificar_user_existente(self, *args):
+        banco_de_dados = self.cliente["ClyviDB"]
+        colecao = banco_de_dados["usuarios"]
+        email = args[0]["e-mail"]
+        resultado = colecao.find_one({"e-mail": email})
+        if resultado == None:
+            return True
+        else:
+            return False
+
+    def verificar_credenciais(self, *args):
+        banco_de_dados = self.cliente["ClyviDB"]
+        colecao = banco_de_dados["usuarios"]
+        print(args)
+        print(*args)
+        email = args[0]
+        senha = args[1]
+        resultado = colecao.find_one({"e-mail": email, "senha": senha})
+        if resultado == None:
+            return False
+        else:
+            return True
 
     def testar_conn(self):
         try:
