ae5dbe7037a06959d4ca4e281ca225dd7091c2ea
Marcos Gabriel de Mesquita Mauricio <mgmesquitamaurcio@gmail.com>
2023-06-27 15:43:13 -0300

    import de arquvios alterados

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from opcoes_do_sistema.entrando_no_sistema import opcoes_na_entrada_do_sistema
+from client.login import opcoes_na_entrada_do_sistema
 
 #testes
 opcoes_na_entrada_do_sistema()
@@ -6,14 +6,3 @@ opcoes_na_entrada_do_sistema()
 # while True:
 #     if opcoes_na_entrada_do_sistema():
 #         opcoes_dentro_do_sistema()
-
-
-
-'''
-Sobre o documento do diagrama
-
-Colocar a descrição do Diagrama de Classes de Projeto do sistema, junto com a figura do diagrama.
-Deve conter todos os elementos da UML. 
-O sistema deverá ter pelo menos 6 classes.
-Se o escopo do problema tratado não incluir este tipo de comportamento, poderá ser expandido
-'''
